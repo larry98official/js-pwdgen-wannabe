1a0f520172bdabcae46667cb17b9c5244c8148cf
Enrico Matricardi <enricomatricardi@hotmail.com>
2020-02-17 16:45:13 +0100

    js-pwdgen-wannabe

---
+++ js/mainscript.js
@@ -0,0 +1,7 @@
+var etaAnagrafica = prompt('Nome?');
+document.getElementById('nome').innerHTML = createPassword;
+var etaAnagrafica = prompt('Cognome?');
+document.getElementById('cognome').innerHTML = createPassword;
+var etaAnagrafica = prompt('Colore preferito?');
+document.getElementById('colorepreferito').innerHTML = createPassword;
+document.getElementById('container').setAttribute('class','visible');
